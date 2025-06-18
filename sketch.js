7342752056f58f553be190fba09418462365921a
Saurinha <sara.rochinski@escola.pr.gov.br>
2025-06-18 11:29:33 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var img01;
+
+function setup() {
+  createCanvas(900,800)
+  img01=loadImage('campo-cuidado-pela-itograss-como-backup-para-times-credito-itograss-1-.avif');
+
+}
+
+function draw() {
+  background(img01)
+  noStroke();
+  fill(20,0,200);
+  textSize(50)
+  text("👨‍🌾", mouseX, mouseY, 50,50);
+  fill(20, 0, 200)
+  textSize(70)
+  text("🌞",750, 100)
+  fill(20, 0, 200)
+  textSize(100)
+  text("🌳",450, 260)
+  fill(20, 0, 200)
+  textSize(100)
+  text("🌳",350, 250)  
+  fill(20, 0, 200)
+  textSize(100)
+  text("🐄",70, 350)
+  fill(20, 0, 200)
+  textSize(70)
+  text("🐓",700, 550)
+  fill(20, 0, 200)
+  textSize(120)
+  text("🚜",600, 400)
+  
+  
+  
+  
+  
+}
+
+
+       
+       
+       
+       
+       
+       
+       
+       
